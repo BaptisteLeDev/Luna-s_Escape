d3f3638e8ba8387f8c3d9c12900b0f05fe67eaad
BaptisteD <baptiste.dechamp@outlook.fr>
2025-02-05 19:15:23 +0100

    Luna's Escape V0.3 - Level 1 Start

--- scenes/salle_1.gd
+++ scenes/salle_1.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
+var is_open = false  # État du coffre (fermé par défaut)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	# Le coffre est fermé au début, donc on masque le sprite "ouvert"
+	$"TombeauFermé".visible = false
 
 
-func _on_next_btn_button_down() -> void:
-	pass # Replace with function body.
+
+func update_coffre_state():
+	if is_open:
+		$TombeauOuvert.visible = false  # Masquer le coffre fermé
+		$"TombeauFermé".visible = true  # Afficher le coffre ouvert
+	else:
+		$TombeauOuvert.visible = true  # Afficher le coffre fermé
+		$"TombeauFermé".visible = false  # Masquer le coffre ouvert
+
+
+func _on_tombeau_ouvert_tombeau_close() -> void:
+	is_open = not is_open
+	update_coffre_state()
